98cb6cfaba3a2134b3c770876060cf138462930b
tibiw <tibor.waxmann@gmail.com>
2022-03-20 15:25:10 +0200

    Initial Commit

---
+++ MySecureApp/MySecureAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySecureAppApp.swift
+//  MySecureApp
+//
+//  Created by Tibor Waxmann on 20.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct MySecureAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
